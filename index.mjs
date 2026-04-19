238d0f1036dbc6d5f2ad8221908fdd0cb8088c29
Juyalu03 <josephlustre90@gmail.com>
2026-04-18 18:07:34 -0700

    NewGui

--- index.mjs
+++ index.mjs
@@ -1,13 +1,22 @@
 import express from "express";
-import "dotenv/config";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
     res.render("index");
+});
+
+app.get("/about", (req, res) => {
+    res.render("about");
 });
 
 app.listen(PORT, () => {
